e524a95f92898afe70b1f8c4b80f497104f1f4c6
Sharath Chandra Linga <sharathchandra.linga@gmail.com>
2023-05-15 22:16:48 +0530

    railway-app-checkin

--- src/main/java/com/org/froom/config/WebConfig.java
+++ src/main/java/com/org/froom/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.org.froom.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,8 +10,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${appName.allowedApi}")
+    private String myAllowedApi;
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**");
+//    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/api/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins(myAllowedApi)
+                    .allowCredentials(false).maxAge(3600);
     }
 }
